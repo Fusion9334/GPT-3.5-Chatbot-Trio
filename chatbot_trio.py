40401ddd2c24e315dc042b849b2e08249b9eab20
Fusion9334 <36902376+Fusion9334@users.noreply.github.com>
2023-04-12 02:22:50 -0400

    Update chatbot_trio.py

--- chatbot_trio.py
+++ chatbot_trio.py
@@ -1,39 +1,44 @@
+from collections import deque
 import openai
 import pyttsx3
 import random
 
 # Set OpenAI API key
-openai.api_key = "API Key"
+openai.api_key = "APY Key"
 MAX_TOKENS = 3750
 MAX_MEMORY_CHARACTERS = 15000
+MAX_MEMORY_MESSAGES = 100
 
-# Function to resize memory to the specified limit while preserving the latest question asked by Model 1
-def resize_memory(memory):
-    total_characters = sum([len(msg["content"]) for msg in memory])
+class LimitedMemoryDeque(deque):
+    def __init__(self, maxlen, max_characters):
+        super().__init__(maxlen=maxlen)
+        self.max_characters = max_characters
+        self.total_characters = 0
 
-    while total_characters > MAX_MEMORY_CHARACTERS:
-        # Remove the oldest message from memory, except the question at index 0
-        if len(memory) > 2:
-            removed_message = memory.pop(1)
-            total_characters -= len(removed_message["content"])
-        else:
-            break
+    def append(self, item):
+        super().append(item)
+        self.total_characters += len(item["content"])
 
-# Function to generate a response from the AI model
-def generate_response(model_memory, user_message, previous_message, model_info):
-    # Add story and humor instructions based on random probability
-    story_instruction = " Include a short story in my response on the subject." if random.random() < 0.5 else ""
+        while self.total_characters > self.max_characters:
+            removed_item = self.popleft()
+            self.total_characters -= len(removed_item["content"])
+
+def adaptive_off_topic_chance(memory):
+    off_topic_base_chance = 0.30
+    recent_questions = [msg["content"] for msg in memory if msg["role"] == "user" and msg["content"].startswith("John,")]
+    off_topic_questions = sum(1 for q in recent_questions if "off-topic" in q)
+    
+    return off_topic_base_chance / 2 if off_topic_questions >= 3 else off_topic_base_chance
+
+def generate_response(memory, user_message, previous_message, model_info):
     humor_instruction = " Include a joke in my response on the subject." if random.random() < 0.15 else ""
+    story_instruction = " Include a short story in my response on the subject." if random.random() < 0.5 else ""
 
-    # Create conversation structure for OpenAI API call
-    conversation = [{"role": "system", "content": f"I am {model_info['name']}, Model {model_info['number']}. My task is to {model_info['role']} and address other models.{humor_instruction}{story_instruction}"}] + model_memory + [user_message, previous_message]
-
-    # Call OpenAI API to generate a response
+    conversation = [{"role": "system", "content": f"I am {model_info['name']}, Model {model_info['number']}. My task is to {model_info['role']} and address other models.{humor_instruction}{story_instruction}"}] + list(memory) + [user_message, previous_message]
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=conversation)
 
     return response['choices'][0]['message']
 
-# Function to speak the text using the specified voice
 def speak(text, voice_id):
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
@@ -41,55 +46,38 @@ def speak(text, voice_id):
     engine.say(text)
     engine.runAndWait()
 
-# New function to handle Model 1 (John)
 def handle_model_1(subject, off_topic_chance):
-    if random.random() < off_topic_chance:
-        content = f"John, please ask an off-topic question on the same subject the other models talked about."
-    else:
-        content = f"John, please ask another question on the subject {subject}."
+    content = f"John, please ask a similar topic question on the same subject the other models talked about." if random.random() < off_topic_chance else f"John, please ask another question on the subject {subject}."
     return content
 
-# New function to handle Model 2 (Sarah)
 def handle_model_2(response_content):
     return f"Sarah, please answer this question: {response_content}"
 
-# New function to handle Model 3 (Michael)
 def handle_model_3(response_content):
     return f"Michael, please give another point of view on this answer: {response_content}"
 
-# Main function to run the chatbot conversation simulation
 def main():
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
 
-    # List available voices
     for index, voice in enumerate(voices):
         print(f"{index}: {voice.id} - {voice.name}")
-
-    # Initialize memory for each chatbot model
-    memories = [[], [], []]
-
-    # Define model information and roles
+    memories = LimitedMemoryDeque(maxlen=MAX_MEMORY_MESSAGES, max_characters=MAX_MEMORY_CHARACTERS)
     model_infos = [
         {"number": 1, "name": "John", "role": "ask questions on the subject", "voice_id": 0},
         {"number": 2, "name": "Sarah", "role": "answer John's questions", "voice_id": 1},
         {"number": 3, "name": "Michael", "role": "analyze Sarah's response and give another point of view on the subject", "voice_id": 2},
     ]
-
-    # Get user input for conversation subject
     subject = input("Enter a conversational subject for the chatbots to talk about: ")
     user_message = {"role": "user", "content": f"John, please write one question on the subject {subject}."}
     previous_message = user_message
     model_number = 0  # Start with the first model (John)
 
-    off_topic_chance = 0.30
-
-    # Continue the conversation simulation
     while True:
-        model_response = generate_response(memories[model_number], user_message, previous_message, model_infos[model_number])
+        model_response = generate_response(memories, user_message, previous_message, model_infos[model_number])
 
-        # Update conversation flow control
         if model_number == 0:
+            off_topic_chance = adaptive_off_topic_chance(memories)
             content = handle_model_1(subject, off_topic_chance)
             model_number = 1
         elif model_number == 1:
@@ -99,22 +87,13 @@ def main():
             content = handle_model_3(model_response['content'])
             model_number = 0
 
-        # Save the response in the model's memory
-        memories[model_number].append(model_response)
+        memories.append(model_response)
 
-        # Resize the memory to limit it to 15,000 characters while preserving the latest question asked by Model 1
-        resize_memory(memories[model_number])
-
-        # Speak the response
         speak(model_response['content'], model_infos[model_number]['voice_id'])
-
-        # Print the response
         print(f"{model_infos[model_number]['name']}: {model_response['content']}")
 
-        # Update the user message for the next model
         user_message = {"role": "user", "content": content}
         previous_message = model_response
 
-# Run the main function to start the chatbot conversation simulation
 if __name__ == "__main__":
     main()
